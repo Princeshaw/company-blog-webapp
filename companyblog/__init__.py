9c36cf299f2796f6ebeb04ef82dbe81bd6243179
Princeshaw <prince54shaw@gmail.com>
2020-10-08 23:57:10 +0530

    model created

--- companyblog/__init__.py
+++ companyblog/__init__.py
@@ -1,6 +1,30 @@
 from flask import Flask,redirect,url_for,render_template,request
+from flask_sqlalchemy import SQLAlchemy
+import os
+from flask_migrate import Migrate
+from flask_login import LoginManager
 
 app=Flask(__name__)
+
+###########################
+###### DATABASE SETUP #####
+###########################
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLAlCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir,'data.sqlite')
+app.config['SQLALCHEM_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+Migrate(app,db)
+######################
+# LOGIN CONFIGS #####
+login_manager = LoginManager()
+
+login_manager.init_app(app)
+login_manager.login_view = 'users.login'
+
+
+
+
+
 
 
 from companyblog.core.views import core
