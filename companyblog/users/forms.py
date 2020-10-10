0627ee9c101f27241785a72fc603b155f8bed24c
Princeshaw <prince54shaw@gmail.com>
2020-10-10 22:20:00 +0530

    Set up user view

--- companyblog/users/forms.py
+++ companyblog/users/forms.py
@@ -1,44 +1,44 @@
-from flask_wtf import FlaskFrom
-from wtforms import StringField, PasswordField, SubmitedField
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Email, EqualTo
-from wtforms import ValidatationError
+from wtforms.validators import ValidationError
 from flask_wtf.file import FileField, FileAllowed
 
 from flask_login import current_user
 from companyblog.models import User
 
-class LoginForm(FlaskFrom):
+class LoginForm(FlaskForm):
     email = StringField('Email',validators=[DataRequired(),Email()])
     password = PasswordField('Password',validators=[DataRequired()])
-    submit  = SubmitedField('Log In')
+    submit  = SubmitField('Log In')
 
-class RegistrationForm(FlaskFrom):
+class RegistrationForm(FlaskForm):
     email = StringField('Email',validators=[DataRequired(),Email()])
     username = StringField('UserName',validators=[DataRequired()])
     password = PasswordField('Password',validators=[DataRequired(),EqualTo('pass_confirm',message='Passwords Must Match')])
     pass_confirm = PasswordField('Confirm Password',validators=[DataRequired()])
-    submit  = SubmitedField('Register')
+    submit  = SubmitField('Register')
 
 
     def check_email(self,field):
         if User.query.filter_by(email=field.data).first():
-            raise ValidatationError('Your email has been registered already!')
+            raise ValidationError('Your email has been registered already!')
 
     def check_username(self,field):
         if User.query.filter_by(username=field.data).first():
-            raise ValidatationError('Your Username has been registred already!')   
+            raise ValidationError('Your Username has been registred already!')   
 
 
-class UpdateUserForm(FlaskFrom):
+class UpdateUserForm(FlaskForm):
     email = StringField('Email',validators=[DataRequired(),Email()])
     username = StringField('UserName',validators=[DataRequired()])
     picture = FileField('Update Profile Picture',validators=[FileAllowed(['jpg','png'])])
-    submit = SubmitedField('Update')
+    submit = SubmitField('Update')
 
     def check_email(self,field):
         if User.query.filter_by(email=field.data).first():
-            raise ValidatationError('Your email has been registered already!')
+            raise ValidationError('Your email has been registered already!')
 
     def check_username(self,field):
         if User.query.filter_by(username=field.data).first():
-            raise ValidatationError('Your Username has been registred already!')
+            raise ValidationError('Your Username has been registred already!')
